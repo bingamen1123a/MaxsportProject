1064f81e1e1b0857dc61d0e9c974c476bed82d87
Hai <haikoi150423@gmail.com>
2024-06-20 10:36:57 +0700

    them hinh

---
+++ MaxsportProject/Controllers/ShopController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaxsportProject.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+    public class ShopDetailController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
